99b36ef6d997acf2f9312ed1826ff3b1c37ef53e
HirokiNakamura <hiroknak@mymac.local>
2014-05-01 21:57:10 +0900

    u

--- app/api/v1/ping.rb
+++ app/api/v1/ping.rb
@@ -54,7 +54,7 @@ module V1
 
           # Create a notification that alerts a message to the user, plays a sound, and sets the badge on the app
           notification = Houston::Notification.new(device: token)
-          #notification.alert = "Hello, World!"
+          notification.alert = "Hello, World!"
 
           # Notifications can also change the badge count, have a custom sound, indicate available Newsstand content, or pass along arbitrary data.
           #notification.badge = 1
